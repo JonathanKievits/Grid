03b830c7ee7459a8375b001462f6f537c3ab9698
JonathanKievits <Jonathan.kievits@hotmail.nl>
2017-01-13 12:17:30 +0100

    opdracht 1

---
+++ Grid/Assets/Level.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour {
+
+    private Grid curGrid;
+
+	void Start ()
+    {
+        curGrid = new Grid(20, 20);
+        curGrid.GetNode(2, 2).IsWalkable = false;
+        curGrid.GetNode(2, 3).IsWalkable = false;
+        curGrid.GetNode(3, 3).IsWalkable = false;
+        curGrid.GetNode(4, 3).IsWalkable = false;
+        curGrid.GetNode(5, 3).IsWalkable = false;
+    }
+	
+
+	void Update ()
+    {
+		
+	}
+}
